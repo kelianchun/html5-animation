05b955ec102cdd9501f256fab22636bb6324d470
kelianchun <kelianchun@qq.com>
2017-06-23 14:37:13 +0800

    add file

--- js/utils.js
+++ js/utils.js
@@ -66,6 +66,28 @@ utils.captureTouch = function (element) {
 };
 
 
+utils.colorRGB = function (color, alpha) {
+    //if string format, convert to number
+    if (typeof color === 'string' && color[0] === '#'){
+        color = window.parseInt(color.slice(1), 16);
+    }
+
+    alpha = (alpha === undefined) ? 1 : alpha;
+
+    var r = color >> 16 & 0xff,
+        g = color >> 8 & 0xff,
+        b = color & 0xff,
+        a = (alpha < 0) ? 0 : ((alpha > 1) ? 1 : alpha); //check range
+
+    //use 'rgba; if needed
+    if (a === 1) {
+        return "rgba(" + r + "," + g + "," + b + ")";
+    } else {
+        return "rgba(" + r + "," + g + "," + b + "," + a + ")";
+    }
+};
+
+
 utils.parseColor = function (color, toNumber) {
   if (toNumber === true) {
     if (typeof color === 'number') {
