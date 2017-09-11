3118a6acdde81c907340df5b149a998413bbcd0f
kelianchun <kelianchun@qq.com>
2017-09-11 11:39:19 +0800

    ch12

--- js/utils.js
+++ js/utils.js
@@ -164,6 +164,25 @@ utils.captureMouse = function (element) {
   return mouse;
 };
 
+utils.colorToRGB = function (color, alpha) {
+  //number in octal format or string prefixed with #
+  if (typeof color === 'string' && color[0] === '#') {
+    color = window.parseInt(color.slice(1), 16);
+  }
+  alpha = (alpha === undefined) ? 1 : alpha;
+  //parse hex values
+  var r = color >> 16 & 0xff,
+      g = color >> 8 & 0xff,
+      b = color & 0xff,
+      a = (alpha < 0) ? 0 : ((alpha > 1) ? 1 : alpha);
+  //only use 'rgba' if needed
+  if (a === 1) {
+    return "rgb("+ r +","+ g +","+ b +")";
+  } else {
+    return "rgba("+ r +","+ g +","+ b +","+ a +")";
+  }
+};
+
 utils.captureTouch = function (element) {
   var touch = {x: null, y: null, isPressed: false, event: null},
       body_scrollLeft = document.body.scrollLeft,
